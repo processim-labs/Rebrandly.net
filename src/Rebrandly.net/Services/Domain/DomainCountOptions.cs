8d6e1036f12440d829b49fb3dc68c18ec5ee773c
Wilson Vargas <wilson.vargas@processimlabs.com>
2020-05-27 22:16:45 -0500

    Add domain service

---
+++ src/Rebrandly.net/Services/Domain/DomainCountOptions.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Rebrandly.Entities.Enums;
+using Rebrandly.Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rebrandly
+{
+    public class DomainCountOptions : BaseOptions
+    {
+        [JsonProperty("active")]
+        public bool Active { get; set; }
+
+        [JsonProperty("type")]
+        public DomainType Type { get; set; }
+    }
+}
---
+++ src/Rebrandly.net/Services/Domain/DomainService.cs
@@ -0,0 +1,45 @@
+﻿using Rebrandly.Entities;
+using Rebrandly.Entities.Base;
+using Rebrandly.Infrastructure.Interfaces;
+using Rebrandly.Services.Base;
+using Rebrandly.Services.Common;
+using Rebrandly.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rebrandly
+{
+    public class DomainService : Service<Domain>,
+        IRetrievable<Domain, DomainGetOption>,
+        IListable<Domain, DomainListOption>,
+        ICountable<Domain, DomainCountOptions>
+    {
+        public DomainService() : base(null)
+        {
+        }
+
+        public DomainService(IRebrandlyClient client) : base(client)
+        {
+        }
+
+        protected override string BasePath => "/domains";
+
+        public virtual Task<RebrandlyCount<Domain>> Count(DomainCountOptions countableOptions, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
+        {
+            return CountEntities(countableOptions, requestOptions, cancellationToken);
+        }
+
+        public virtual Task<Domain> Get(string id, DomainGetOption retrieveOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
+        {
+            return GetEntity(id, retrieveOptions, requestOptions, cancellationToken);
+        }
+
+        public virtual Task<RebrandlyList<Domain>> List(DomainListOption listOptions = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default)
+        {
+            return ListEntities(listOptions, requestOptions, cancellationToken);
+        }
+    }
+}
